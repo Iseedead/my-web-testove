200be8d4a90c1f72d4595a633433c6e52fd4c5b4
Pasha S <sivkovych.p@gmail.com>
2017-10-31 21:18:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# front-silex
+Skeleton: Silex framework php and webpack + es6 + sass architecture frontend demo
+
+##requisites
+- php5 installed
+- composer installed
+- node installed
+- npm installed
+
+##run
+install dependencies
+
+backend
+```
+composer install
+```
+
+front 
+```
+npm install && npm run start
+```
+enjoy
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+/**
+ * @author Juan Pablo <juanpablocs21@gmail.com
+ * @date 16/09/16.
+ */
+ 
+var webpack = require('webpack');
+
+module.exports = {
+    context : __dirname + '/frontend/src',
+    entry   : {
+        app     : __dirname + '/frontend/src/main.js',
+        vendor : ['underscore', 'jp-router']
+    },
+    output  : {
+        path      : __dirname + '/web/assets/',
+        publicPath: '/assets/',
+        filename  : '[name].min.js',
+        chunkFilename: '[id].[name].min.js'
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin('vendor', '[name].min.js')
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: "babel",
+                query:{presets:['es2015']},
+                exclude:/node_modules/
+            },            {
+                test: /\.vue$/,
+                loader: 'vue'
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.js'
+        }
+    }
+};
